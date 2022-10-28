ebaf5bc6dbd4d49ac9d55d7712da605aa8cf265c
gueng_nen <nabin111@naver.com>
2022-10-29 02:53:01 +0900

    chore: debugging

--- app/src/main/java/edu/skku/cs/dokkang/MainActivity.java
+++ app/src/main/java/edu/skku/cs/dokkang/MainActivity.java
@@ -1,11 +1,27 @@
 package edu.skku.cs.dokkang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +38,83 @@ public class MainActivity extends AppCompatActivity {
         Button loginButton = findViewById(R.id.loginButton);
         /* login - onClick */
         loginButton.setOnClickListener(view -> {
+            String id = idEditText.getText().toString();
+            String pw = pwEditText.getText().toString();
 
+            if (id.equals("") || pw.equals("")) {
+                Toast.makeText(getApplicationContext(), "Please fill in the blanks", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            OkHttpClient client = new OkHttpClient();
+
+            LoginDataModel data = new LoginDataModel();
+            data.setUsername(id);
+            data.setPassword(pw);
+
+            Gson gson = new Gson();
+            String json = gson.toJson(data, LoginDataModel.class);
+
+            HttpUrl.Builder urlBuilder = HttpUrl.parse("http://db.dokkang.tk:8080").newBuilder();
+            String url = urlBuilder.build().toString();
+
+            Request req = new Request.Builder().url(url).post(RequestBody.create(MediaType.parse("application/json"), json)).build();
+
+            client.newCall(req).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    /* response */
+                    final String res = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), res, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    Gson gson = new GsonBuilder().create();
+                    final LoginResponseDataModel data = gson.fromJson(res, LoginResponseDataModel.class);
+
+                    if (data.getStatus() == "ok") {
+                        String message = "Login success";
+
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        Intent intent = new Intent(MainActivity.this, SignUp.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        final int status = response.code();
+                        String message = "";
+
+                        if (status == 404) {
+                            message += "User id does not exist";
+                        } else if (status == 401) {
+                            message += "Password not matched";
+                        } else {
+                            message += "Login failed";
+                        }
+
+                        String finalMessage = message;
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), finalMessage, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                }
+            });
         });
 
         /* signup button */
---
+++ app/src/main/java/edu/skku/cs/dokkang/MyPage.java
@@ -0,0 +1,14 @@
+package edu.skku.cs.dokkang;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
