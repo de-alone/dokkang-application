85c61b5f5f8a2b416556e22c7da4317f674ee1ff
gueng_nen <nabin111@naver.com>
2022-10-29 10:44:29 +0900

    feat: add sign up functionality using API

---
+++ app/src/main/java/edu/skku/cs/dokkang/SignUpResponseDataModel.java
@@ -0,0 +1,13 @@
+package edu.skku.cs.dokkang;
+
+public class SignUpResponseDataModel {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
