3fe4dc302975965c5033b1d64894150a6445a54b
xi-one <ssly1133@skku.edu>
2022-10-30 20:18:44 +0900

    feat: add button events in mypage

--- app/src/main/java/edu/skku/cs/dokkang/MyPage.java
+++ app/src/main/java/edu/skku/cs/dokkang/MyPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,13 +13,18 @@ public class MyPage extends AppCompatActivity {
 
     private ListView listView;
 
-    private MyLectureListViewAdapter listViewAdapter;
-    private ArrayList<Lecture> items;
+    private MySubjectListViewAdapter listViewAdapter;
+    private ArrayList<Subject> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        Button subject_edit_btn = findViewById(R.id.editSubjectbutton);
+        Button personalInfo_btn = findViewById(R.id.pInfobutton);
+        Button logout_btn = findViewById(R.id.LogOutbutton);
+        listView = findViewById(R.id.commentListView);
 
         Intent intent = getIntent();
         String token = intent.getStringExtra("token");
@@ -26,20 +32,36 @@ public class MyPage extends AppCompatActivity {
 
 
         /* 서버에 강의 목록 요청해서 items arraylist에 삽입*/
+
+        // test set  (나중에 삭제해주세요)
         items = new ArrayList<>();
-        Lecture TestLecture1 = new Lecture("인공지능개론", "박진영", 1, 1);
-        Lecture TestLecture2 = new Lecture("소프트웨어공학개론", "차수영", 2, 2);
+        Subject TestLecture1 = new Subject("인공지능개론", "박진영", 1, 1);
+        Subject TestLecture2 = new Subject("소프트웨어공학개론", "차수영", 2, 2);
         items.add(TestLecture1);
         items.add(TestLecture2);
         /* 서버에 강의 목록 요청해서 items arraylist에 삽입*/
 
-        listView = findViewById(R.id.commentListView);
-        listViewAdapter = new MyLectureListViewAdapter(items, getApplicationContext());
 
+        /* show list of my lectures*/
+        listViewAdapter = new MySubjectListViewAdapter(items, getApplicationContext());
         listView.setAdapter(listViewAdapter);
 
+        /*subject edit button click events*/
+        subject_edit_btn.setOnClickListener(view -> {
+            Intent se_intent = new Intent(MyPage.this, SubjectEdit.class);
+            se_intent.putExtra("user_id", user_id);
+            se_intent.putExtra("token", token);
+            startActivity(se_intent);
 
+        });
 
+        /*personal infomation button click events*/
+        personalInfo_btn.setOnClickListener(view -> {
+            Intent pinfo_intent = new Intent(MyPage.this, PersonalInfo.class);
+            pinfo_intent.putExtra("user_id", user_id);
+            pinfo_intent.putExtra("token", token);
+            startActivity(pinfo_intent);
+        });
 
     }
 }
