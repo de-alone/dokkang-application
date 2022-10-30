4848e28d8838ef2633a0aa517189594fca8f097c
xi-one <ssly1133@skku.edu>
2022-10-30 23:35:12 +0900

    feat: show list of all subjects in edit subject activity

---
+++ app/src/main/java/edu/skku/cs/dokkang/EditSubjectListViewAdapter.java
@@ -0,0 +1,85 @@
+package edu.skku.cs.dokkang;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+class Subject {
+    public String lecture_name;
+    public String prefessor_name;
+    public int lecture_id;
+    public int lecture_no;
+    public boolean checked;
+
+    public Subject(String lecture_name, String prefessor_name, int lecture_id, int lecture_no, boolean checked) {
+        this.lecture_name = lecture_name;
+        this.prefessor_name = prefessor_name;
+        this.lecture_id = lecture_id;
+        this.lecture_no = lecture_no;
+        this.checked = checked;
+    }
+}
+
+
+public class EditSubjectListViewAdapter extends BaseAdapter {
+
+    private ArrayList<Subject> item;
+    private Context mContext;
+
+
+    public EditSubjectListViewAdapter(ArrayList<Subject> item, Context mContext) {
+        this.item = item;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return item.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return item.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.edit_subject_list, viewGroup, false);
+        }
+
+        TextView lecture_name = view.findViewById(R.id.subjectName_text);
+        TextView lecture_no = view.findViewById(R.id.LectureNum_text);
+        TextView professor = view.findViewById(R.id.ProfessorName_text);
+        CheckBox checkBox = view.findViewById(R.id.checkBox);
+
+        /*show lecture info */
+        lecture_name.setText(item.get(i).lecture_name);
+        lecture_no.setText(Integer.toString(item.get(i).lecture_no));
+        professor.setText(item.get(i).prefessor_name);
+
+        /*check lectures */
+        checkBox.setChecked(item.get(i).checked);
+        checkBox.setOnClickListener(v -> {
+            item.get(i).checked = checkBox.isChecked();
+        });
+
+
+        return view;
+    }
+}
--- app/src/main/java/edu/skku/cs/dokkang/SubjectEdit.java
+++ app/src/main/java/edu/skku/cs/dokkang/SubjectEdit.java
@@ -2,13 +2,66 @@ package edu.skku.cs.dokkang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SubjectEdit extends AppCompatActivity {
+
+    private ListView listView;
+    private EditSubjectListViewAdapter listViewAdapter;
+    private ArrayList<Subject> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_edit);
+
+        Intent intent = getIntent();
+        String token = intent.getStringExtra("token");
+        long user_id = intent.getLongExtra("user_id",0);
+
+        Button confirm_btn = findViewById(R.id.confirmButton);
+        listView = findViewById(R.id.subjectListView);
+
+
+        /* 서버에 강의 목록 요청해서 items arraylist에 삽입*/
+
+        // test set  (나중에 삭제해주세요)
+        items = new ArrayList<>();
+        Subject TestLecture1 = new Subject("인공지능개론", "박진영", 1, 1, false);
+        Subject TestLecture2 = new Subject("소프트웨어공학개론", "차수영", 2, 2, false);
+        items.add(TestLecture1);
+        items.add(TestLecture2);
+        /* 서버에 강의 목록 요청해서 items arraylist에 삽입*/
+
+
+        /* show list of my lectures*/
+        listViewAdapter = new EditSubjectListViewAdapter(items, getApplicationContext());
+        listView.setAdapter(listViewAdapter);
+
+        /*confirm button*/
+        confirm_btn.setOnClickListener(view -> {
+            /*items 안의 checked 확인해서 check된 과목들을 서버에 등록 요청보냄*/
+            // 아래 코드는 제대로 작동하는지 테스트용으로 작성했습니다. 서버 요청 구현할 떄 삭제해주세요
+            String message = "";
+            Log.i("debug", "check");
+            for(int i = 0; i < items.size(); i++) {
+                if(items.get(i).checked) {
+                    Log.i("debug", "check");
+                    message = message + items.get(i).lecture_name;
+                    message = message + ", ";
+                }
+            }
+            Toast.makeText(SubjectEdit.this, message, Toast.LENGTH_SHORT).show();
+            /*items 안의 checked 확인해서 check된 과목들을 서버에 등록 요청보냄*/
+
+            finish();
+        });
     }
 }
