28dbed024c020bbae7416fd7072628484bfa1cb1
xi-one <ssly1133@skku.edu>
2022-10-30 19:46:30 +0900

    feat: show list of my lectures in mypage

--- app/src/main/java/edu/skku/cs/dokkang/MyPage.java
+++ app/src/main/java/edu/skku/cs/dokkang/MyPage.java
@@ -2,13 +2,44 @@ package edu.skku.cs.dokkang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyPage extends AppCompatActivity {
+
+    private ListView listView;
+
+    private MyLectureListViewAdapter listViewAdapter;
+    private ArrayList<Lecture> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        Intent intent = getIntent();
+        String token = intent.getStringExtra("token");
+        long user_id = intent.getLongExtra("user_id",0);
+
+
+        /* 서버에 강의 목록 요청해서 items arraylist에 삽입*/
+        items = new ArrayList<>();
+        Lecture TestLecture1 = new Lecture("인공지능개론", "박진영", 1, 1);
+        Lecture TestLecture2 = new Lecture("소프트웨어공학개론", "차수영", 2, 2);
+        items.add(TestLecture1);
+        items.add(TestLecture2);
+        /* 서버에 강의 목록 요청해서 items arraylist에 삽입*/
+
+        listView = findViewById(R.id.commentListView);
+        listViewAdapter = new MyLectureListViewAdapter(items, getApplicationContext());
+
+        listView.setAdapter(listViewAdapter);
+
+
+
+
     }
 }
---
+++ app/src/main/java/edu/skku/cs/dokkang/SubjectEdit.java
@@ -0,0 +1,14 @@
+package edu.skku.cs.dokkang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubjectEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_edit);
+    }
+}
